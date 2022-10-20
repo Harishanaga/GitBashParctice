f9bc54e7d98f1d598de69f543c3849fe8a79bf38
Harishanaga <nagaharisha99@gmail.com>
2022-10-20 11:46:44 +0530

    2nd class added

---
+++ src/test/java/GitManjuPac/GitManjuClass2.java
@@ -0,0 +1,10 @@
+package GitManjuPac;
+
+public class GitManjuClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello manju");
+	}
+
+}
